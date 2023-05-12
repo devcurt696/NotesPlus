02826e411fd85711e690e102f2e15c3d88250207
Curtis Crouse <72637696+curtcurt69@users.noreply.github.com>
2023-05-11 20:00:13 -0400

    more style changes

--- resources/views/notes/edit.blade.php
+++ resources/views/notes/edit.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-white leading-tight">
             {{ __('Edit') }}
         </h2>
     </x-slot>
@@ -9,14 +9,14 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             
             
-                <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
+                <div class="my-6 p-6 bg-blue-600 border-b border-gray-200 shadow-sm sm:rounded-lg">
                     <form action="{{ route('notes.update', $note)}}" method="post">
                         @method('put')
                         @csrf
                         <x-input type="text" name="title" placeholder="Enter a title..." class="w-full" autocomplete="off" :value="@old('title', $note->title)"></x-input>
                         <x-textarea name="text" rows="10" placeholder="Enter text..." class="w-full" :value="@old('text', $note->text)"></x-textarea>
                         
-                        <button class="mt-6 bg-slate-900 text-white rounded-md p-1">Save Note</button>
+                        <button class="mt-6 bg-slate-500 text-white rounded-md p-1">Save Note</button>
                     </form>
                 </div>
             
